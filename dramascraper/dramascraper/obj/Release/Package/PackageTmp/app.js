e7eb9e6886ed2025a2f2b9ba85787fb31d681568
Ayaz Ahmad Pikessoft <ayaz.ahmad@pikessoft.com>
2016-11-26 00:04:20 +0500

    Scraper to get list of all channels and their sublink

---
+++ dramascraper/dramascraper/obj/Release/Package/PackageTmp/app.js
@@ -0,0 +1,94 @@
+﻿var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var request = require('request');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var urlModule = require('url');
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', users);
+
+app.get('/getData', function (req, res) {
+    
+    var typeValue = req.query.type;
+    var urlValue = req.query.url;
+     
+    var resultsFromFunction;
+    request(urlValue, function (error, response, html) {
+        
+        if (!error && response.statusCode === 200) {
+            if (typeValue.toString().match('channellist')) {
+                resultsFromFunction = users.channelList(html);
+            }
+            else if (typeValue.toString().match('dramalist')) {
+                resultsFromFunction = users.dramaList(html);
+            }
+            else if (typeValue.toString().match('epilist')) {
+                resultsFromFunction = users.epiList(html);
+            }
+            else if (typeValue.toString().match('epilink')) {
+                resultsFromFunction = users.epiLink(html);
+            }
+            
+            
+            res.end(JSON.stringify(resultsFromFunction));
+
+        }
+        else {
+            console.log('\nShowing error...\n');
+            console.log(error);
+        }
+            
+    });
+   
+});
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+module.exports = app;
---
+++ dramascraper/dramascraper/obj/Release/Package/PackageTmp/routes/users.js
@@ -0,0 +1,120 @@
+﻿var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var router = express.Router();
+
+
+//var mainUrl = "http://dramaonline.com/";
+var mainUrl = "http://dramaonline.com/geo-tv-latest-dramas-episodes-online/"
+//var mainUrl = "http://dramaonline.com/category/diyar-e-dil/"
+//var mainUrl = "http://dramaonline.com/ishqa-waay-episode-32-in-hd/"
+
+
+router.get('/', function (req, res) {
+    request(mainUrl, function (error, response, html) {
+        if (!error && response.statusCode === 200) {
+            // var channelResult = channelList(html);
+            var dramaResult = dramaList(html);
+            //var epiResut = epiList(html);
+            //  var epilink = epiLink(html);
+            res.send(dramaResult);
+        }
+        return console.log(error);
+    });
+});
+
+var channelList = function (html) {
+    var channellist = {};
+    
+    var i = 0;
+    var $ = cheerio.load(html);
+    $('ul.channels-list-area>li').each(function (i, element) {
+        var data1 = $(this);
+        var channelName = data1.children().text();
+        var channelImage = data1.children().children().attr("src");
+        var channellink = data1.children().attr("href");
+        
+        list = {
+            channelName: channelName,
+            
+            channelImage: channelImage,
+            channellink: channellink,
+    
+        };
+        channellist[i] = list;
+        i++;
+    })
+    
+    return channellist;
+};
+var dramaList = function (html) {
+    var daramas = {};
+    
+    var i = 0;
+    var $ = cheerio.load(html);
+    
+    $('.thecontent>table').filter(function () {
+        var daramaList = {};
+        var data1 = $(this);
+        $('tr>td>a').each(function (i) {
+            var data2 = $(this);
+            var daramaImage = data2.children().first().attr("src");
+            var dramaLink = data2.attr("href");
+            dataDrama = {
+                daramaImage: daramaImage,
+                dramaLink: dramaLink,         
+            };
+            daramas[i] = dataDrama;
+            i++;
+        });
+    
+    })
+    return daramas;
+};
+var epiList = function (html) {
+    var epilistJson = {};
+    
+    var i = 0;
+    var $ = cheerio.load(html);
+    
+    
+    $('.latestPost-content').each(function (i, element) {
+        var epiArray = $(this);
+        var episodeLink = epiArray.children().first().attr("href");
+        var episodeName = epiArray.children().first().attr("title");
+        var episodeImage = epiArray.children().children().children().first().attr("src");
+        episodes = {
+            episodelink: episodeLink,            
+            episodename: episodeName,  
+        };
+        epilistJson[i] = episodes;
+        i++;
+    })
+    return epilistJson
+};
+var epiLink = function (html) {
+    var elink = {};
+    var i = 0;
+    var $ = cheerio.load(html);
+    $('.thecontent').each(function (i, element) {
+        
+        var iframe = $('iframe');
+        var play = iframe.attr("src");
+        
+        video = {
+            videolink: play,            
+           
+        };
+        elink = video;
+        i++;
+    })
+    
+    return elink;
+
+};
+
+module.exports = router;
+module.exports.channelList = channelList;
+module.exports.dramaList = dramaList;
+module.exports.epiList = epiList;
+module.exports.epiLink = epiLink;
